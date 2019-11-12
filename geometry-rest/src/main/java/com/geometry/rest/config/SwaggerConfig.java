009728bf5f9d1d29cb2bb88795e9990960c2cc2c
xiaoqiaohui <1187773467@qq.com>
2019-11-12 18:36:06 +0800

    优化

--- geometry-rest/src/main/java/com/geometry/rest/config/SwaggerConfig.java
+++ geometry-rest/src/main/java/com/geometry/rest/config/SwaggerConfig.java
@@ -10,14 +10,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
-*
-* @Description:
-* @author zp
-* @email zp152527@163.com
-* @date 2018/8/9 13:49 
-*
-*/
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
